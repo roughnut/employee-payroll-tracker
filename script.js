bc9022958e67e7a255e3950d4e36407255a7f6d3
roughnut <jeremy@writever.com.au>
2024-03-21 11:19:37 +1100

    commit commented script.js file

--- script.js
+++ script.js
@@ -4,27 +4,34 @@ const addEmployeesBtn = document.querySelector('#add-employees-btn');
 // Collect employee data
 const collectEmployees = function() {
   // TODO: Get user input to create and return an array of employee objects
+  // Create an empty locally scoped array to store employee data
   const employeesArray = [];
   
+  // Set a flag to determine if the user wants to continue adding employees
   let continueAdding = true;
 
+  // Prompt the user to enter employee data
   while (continueAdding) {
   const firstName = prompt('Employee\'s first name?');
   const lastName = prompt('Employee\'s last name?');
   let salary = prompt('Employee\'s salary?');
 
+  // If salary is not a number, set it to 0
   if (isNaN(salary)) {
     salary = 0;
   }
 
+  // Create an object to store the employee data
   const employeeDetails = {
     firstName: firstName,
     lastName: lastName,
     salary: salary
   };
   
+// Push the employee data to the employeesArray
   employeesArray.push(employeeDetails);
 
+  // Ask the user if they want to continue adding employees
   continueAdding = confirm('Enter another employee?');
   if (!continueAdding) {
     return employeesArray;
@@ -35,20 +42,34 @@ const collectEmployees = function() {
 // Display the average salary
 const displayAverageSalary = function(employeesArray) {
   // TODO: Calculate and display the average salary
-  console.log(`The number of employees is: ${employeesArray.length}`);
+
+  // Create a variable to store the total salary
   let totalSalary = 0;
+
+  // Console log the number of employees
+  console.log(`The number of employees is: ${employeesArray.length}`);
+
+  // Create a loop to calculate the total salary - Need to convert the salary to a number and add to totalSalary
   for (let i = 0; i < employeesArray.length; i++) {
     totalSalary += Number(employeesArray[i].salary);
   }
+
+  // Console log the total salary and the average salary - round the average salary to the nearest integer
   console.log(`The sum of all salaries is: ${totalSalary}`);
-  console.log(`The average salary is: ${Math.round(totalSalary / employeesArray.length)}`);
+  console.log(
+    `The average salary is: ${Math.round(totalSalary / employeesArray.length)}`
+  );
   return;
 }
 
 // Select a random employee
 const getRandomEmployee = function(employeesArray) {
   // TODO: Select and display a random employee
+
+  // Generate a random number between 0 and the length of the employeesArray
   const randomEmployeeNumber = Math.floor(Math.random() * employeesArray.length);
+
+  // Console log the random employee' first and last names extracted from the object in the employeesArray
   console.log(`Random employee of the month: ${employeesArray[randomEmployeeNumber].firstName} ${employeesArray[randomEmployeeNumber].lastName}! Their prize is a prominently featured console.log. Wooooo!`);
 }
 
